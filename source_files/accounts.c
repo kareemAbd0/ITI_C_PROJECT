5b2bbe72bfdd0c481c6bc3da37a2970162f43ea0
kareem <kareemabdo.7781@yahoo.com>
2025-08-02 21:13:20 +0300

    login fix

--- source_files/accounts.c
+++ source_files/accounts.c
@@ -1,91 +1,67 @@
-//
-// Created by kareem on 8/2/25.
-//
-
-
 #include <string.h>
-#include"../headers/file_handler.h"
+#include <stdio.h>
+#include "../headers/file_handler.h"
 #include "../headers/GUI.h"
 #include "../headers/accounts.h"
-#include <stdio.h>
 
-
-
-
-void strip_newline(char *str){
+void strip_newline(char *str) {
     int len = strlen(str);
     if (len > 0 && str[len - 1] == '\n') {
-	str[len - 1] = '\0';
+        str[len - 1] = '\0';
     }
 }
 
-
-
 int login(Users *my_user_data) {
+    char username[20] = "";
+    char password[20] = "";
 
-	int option = display_combined_box(empty_array, 0, login_screen, 4);
+    const char *static_username_line[] = {"Enter username:"};
+    const char *static_password_line[] = {"Enter password:"};
+    const char *static_notfound_line[] = {"Username or password incorrect."};
 
-	const char *static_username_line[] = {"enter username",};
-	const char *static_password_line[] = {"enter password",};
-	const char *static_notfound_line[] = {"username or password incorrect",};
-	char username[20];
-	char password[20];
+    int option = display_combined_box(empty_array, 0, login_screen, 4);
 
-	while (option != 4){
-		if (option == 1) {
+    while (option != 4) {
+        if (option == 1) {
+            // Input username
+            display_combined_box(static_username_line, 1, empty_array, 0);
+            fgets(username, sizeof(username), stdin);
+            strip_newline(username);
 
-			display_combined_box(static_username_line,1, empty_array, 0);
-			fgets(username, sizeof(username),stdin);
-			strip_newline(username); 
-			display_combined_box(empty_array, 0, login_screen, 4);
+        } else if (option == 2) {
+            // Input password
+            display_combined_box(static_password_line, 1, empty_array, 0);
+            fgets(password, sizeof(password), stdin);
+            strip_newline(password);
 
+        } else if (option == 3) {
+            // Attempt login
+            strcpy(my_user_data->username, username);
+            strcpy(my_user_data->password, password);
 
-		}else if (option == 2) {
-			display_combined_box(static_password_line,1, empty_array, 0);
-			fgets(password, sizeof(password), stdin);
-			strip_newline(password);
-			display_combined_box(empty_array, 0, login_screen, 4);
-		}else if (option == 3) {
+            int found = 0;
+            for (int i = 0; i < user_count; i++) {
+                if (strcmp(username, user_data[i].username) == 0 &&
+                    strcmp(password, user_data[i].password) == 0) {
+                    found = 1;
 
-			strcpy(my_user_data->username, username);
-			strcpy(my_user_data->password, password);
+                    if (user_data[i].is_admin) {
+                        return display_combined_box(empty_array, 0, Admin_main_screen, 4);
+                    } else {
+                        return display_combined_box(empty_array, 0, User_main_screen, 4);
+                    }
+                }
+            }
 
+            if (!found) {
+                option = display_combined_box(static_notfound_line, 1, login_screen, 4);
+                continue;
+            }
+        }
 
-			for (int i = 0; i < user_count; i++) {
+        option = display_combined_box(empty_array, 0, login_screen, 4);
+    }
 
-				if (strcmp(my_user_data->username, user_data[i].username) == 0) {
-
-					if (strcmp(my_user_data->password, user_data[i].password) == 0) {
-
-						int admin_status = user_data[i].is_admin;
-						if (admin_status == 0) {
-
-							return	display_combined_box(empty_array, 0, User_main_screen, 4);
-
-
-						} else if (admin_status == 1) {
-
-							return  display_combined_box(empty_array, 0, Admin_main_screen, 4);
-
-						}
-
-
-					}else{
-						option = display_combined_box(static_notfound_line, 1, login_screen, 4);
-						continue;
-					}
-				}
-			}
-
-			option = display_combined_box(static_notfound_line, 1, login_screen, 4);
-
-		}
-	}     
-
-	return display_combined_box(empty_array, 0, start_menu, 3);
-
+    // If user chose to go back
+    return display_combined_box(empty_array, 0, start_menu, 3);
 }
-
-
-
-
