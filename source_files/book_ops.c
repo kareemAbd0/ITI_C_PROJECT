08cf8f3ad0084ab54703a5a24ce1cfb043b046aa
YoussefAboud <youssefaboud44@gmail.com>
2025-08-04 23:54:26 +0300

    edit ops

--- source_files/book_ops.c
+++ source_files/book_ops.c
@@ -28,15 +28,17 @@ int return_book(Book books[], int size, char title[]){
     return -1;
 }
 
-int  add_book(Book books[],int size,  Book book){
+int  add_book(Book books[],int size, Book book){
     for(int i=0;i< size;i++){
-        if(books[i].isbn == 0){
-            books[i] = book;
+        if(strcmp(books[i].isbn, book.isbn)==0){
+            books[i].number_copies+=book.number_copies;
             return 0;
         }
-        else if(strcmp(books[i].isbn, book.isbn)==0){
-            books[i].number_copies+=book.number_copies;
-        return 0;
+    }
+    for(int i=0;i< size;i++){
+        if(books[i].isbn == '0'){
+            books[i] = book;
+            return 0;
         }
     }
     return -1;
@@ -56,19 +58,21 @@ int  delete_book(Book books[],int size,  char isbn[]){
 int search_book_by_isbn(char *isbn,Book books[], int size){
     for(int i=0;i< size;i++){
         if(strcmp(books[i].isbn, isbn)==0){
-            return books[i].number_copies;
+            return i;
         }
     }
-    return 0;
+    return -1;
 }
+
 int search_book_by_title(char *title,Book *books, int size){
     for(int i=0;i< size;i++){
         if(strcmp(books[i].title, title)==0){
-            return books[i].number_copies;
+            return i;
         }
     }
-    return 0;
+    return -1;
 }
+
 void search_book_by_author(char *author,Book *books, int size, int indicies[]){
     int counter = 0;
     for(int i=0;i< size;i++){
@@ -78,6 +82,7 @@ void search_book_by_author(char *author,Book *books, int size, int indicies[]){
         }
     }
 }
+
 void search_book_by_year(int year,Book *books, int size, int indicies[]){
     int counter = 0;
     for(int i=0;i< size;i++){
